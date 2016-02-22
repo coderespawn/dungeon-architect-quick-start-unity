d58d7ec2f503e49a72b31ed53fef7365e96f6c40
Ali Akbar <ali.akbar@gmail.com>
2016-02-22 18:40:16 +0530

    Added more demos for indoor theme and volumes

---
+++ Support/Scripts/Rules/NonStairTileSelectorRule.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using DungeonArchitect;
+using DungeonArchitect.Utils;
+
+public class NonStairTileSelectorRule : NonViewBlockingSelectionRule
+{
+    public override bool CanSelect(PropSocket socket, Matrix4x4 propTransform, DungeonModel model, System.Random random)
+    {
+        var selected = base.CanSelect(socket, propTransform, model, random);
+        if (!selected) return false;
+
+        // Further filter near the door positions
+        var cellId = socket.cellId;
+        if (model is GridDungeonModel)
+        {
+            var gridModel = model as GridDungeonModel;
+            
+            var position = Matrix.GetTranslation(ref propTransform);
+            var gridSize = gridModel.Config.GridCellSize;
+            var x = Mathf.FloorToInt(position.x / gridSize.x);
+            var z = Mathf.FloorToInt(position.z / gridSize.z);
+            return !gridModel.ContainsStairAtLocation(x, z);
+        }
+
+        return false;
+    }
+
+}
