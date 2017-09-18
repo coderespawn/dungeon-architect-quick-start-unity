a6a8d0cc31964c51568a4e0f0e095fc2bc61df63
Ali Akbar <ali.akbar@coderespawn.com>
2017-09-18 17:04:53 +0530

    Added a new demo for room ceilings in the candy theme

---
+++ Support/Scripts/Rules/NonStairRoomTileSelectorRule.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using DungeonArchitect;
+using DungeonArchitect.Utils;
+using DungeonArchitect.Builders.Grid;
+
+public class NonStairRoomTileSelectorRule : NonViewBlockingSelectionRule
+{
+    public override bool CanSelect(PropSocket socket, Matrix4x4 propTransform, DungeonModel model, System.Random random)
+    {
+        var selected = base.CanSelect(socket, propTransform, model, random);
+        if (!selected) return false;
+
+        // Further filter near the door positions
+        if (model is GridDungeonModel)
+        {
+            var gridModel = model as GridDungeonModel;
+            var position = Matrix.GetTranslation(ref propTransform);
+            var gridSize = gridModel.Config.GridCellSize;
+            var x = Mathf.FloorToInt(position.x / gridSize.x);
+            var z = Mathf.FloorToInt(position.z / gridSize.z);
+            var cellInfo = gridModel.GetGridCellLookup(x, z);
+            bool isRoom = cellInfo.CellType == CellType.Room;
+            bool containsStair = gridModel.ContainsStairAtLocation(x, z);
+            return isRoom && !containsStair;
+        }
+
+        return false;
+    }
+
+}
