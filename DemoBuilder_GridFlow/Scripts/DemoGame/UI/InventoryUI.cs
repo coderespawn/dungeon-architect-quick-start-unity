5f20882e4f7b6e54d9d62f1c91607de0606e412d
Ali Akbar <ali.akbar@gmail.com>
2019-12-01 05:49:32 +0530

    Added inventory and key lock system in the grid flow demo

---
+++ DemoBuilder_GridFlow/Scripts/DemoGame/UI/InventoryUI.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DungeonArchitect.Samples.GridFlow
+{
+    public class InventoryUI : MonoBehaviour
+    {
+        public Image[] slotImages;
+
+        public void UpdateUI(Inventory inventory)
+        {
+            for (int i = 0; i < inventory.slots.Length && i < slotImages.Length; i++)
+            {
+                var slot = inventory.slots[i];
+                if (slot.item.itemType != InventoryItemType.None)
+                {
+                    var image = slotImages[i];
+                    image.sprite = slot.item.icon;
+                    image.color = Color.white;
+                }
+            }
+        }
+    }
+}
