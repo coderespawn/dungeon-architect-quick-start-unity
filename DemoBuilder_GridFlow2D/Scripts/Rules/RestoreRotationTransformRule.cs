3e16fc01f5924b6a08ab912a52f5a3e30caa6e79
Ali Akbar <ali.akbar@coderespawn.com>
2019-12-03 17:08:20 +0530

    Added a basic 2d gridflow sample

---
+++ DemoBuilder_GridFlow2D/Scripts/Rules/RestoreRotationTransformRule.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DungeonArchitect;
+
+namespace DungeonArchitect.Samples.GridFlow
+{
+    public class RestoreRotationTransformRule : TransformationRule
+    {
+        public override void GetTransform(PropSocket socket, DungeonModel model, Matrix4x4 propTransform, System.Random random, out Vector3 outPosition, out Quaternion outRotation, out Vector3 outScale)
+        {
+            base.GetTransform(socket, model, propTransform, random, out outPosition, out outRotation, out outScale);
+            var rotation = socket.Transform.rotation;
+            outRotation = Quaternion.Inverse(rotation);
+        }
+    }
+}
