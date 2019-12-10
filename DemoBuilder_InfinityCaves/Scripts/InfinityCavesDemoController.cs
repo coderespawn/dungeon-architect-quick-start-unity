d080185b78feae77a4bf8969c9ba4ddcb3a595d4
Ali Akbar <ali.akbar@coderespawn.com>
2019-12-10 15:58:29 +0530

    Added new content

---
+++ DemoBuilder_InfinityCaves/Scripts/InfinityCavesDemoController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DungeonArchitect.Samples.InfinityCaves
+{
+    public class InfinityCavesDemoController : MonoBehaviour
+    {
+        public InfinityDungeon dungeon;
+
+        void Start()
+        {
+            if (dungeon != null)
+            {
+                dungeon.BuildDungeon();
+            }
+        }
+
+    }
+}
