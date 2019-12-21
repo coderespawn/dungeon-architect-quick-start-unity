c21ceabd03d562534cda2ee46dad98559306d45b
Ali Akbar <ali.akbar@coderespawn.com>
2019-12-21 11:21:10 +0530

    Added internal lighting mapping dev tools

---
+++ Support/DeveloperTools/Editor/SceneLightmapBaker.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+namespace DungeonArchitect.Editors.DevTools
+{
+    public class SceneLightmapBaker
+    {
+        static void SetupLightSettings()
+        {
+            Lightmapping.realtimeGI = false;
+            Lightmapping.bakedGI = false;
+        }
+
+
+        [MenuItem("Dungeon Architect/Internal Dev Tools/Lightmap/Clear Current", priority = 2001)]
+        public static void ClearCurrentScene()
+        {
+            Lightmapping.Clear();
+            Lightmapping.ClearLightingDataAsset();
+        }
+
+        [MenuItem("Dungeon Architect/Internal Dev Tools/Lightmap/Bake Current", priority = 2002)]
+        public static void BakeCurrentScene()
+        {
+            SetupLightSettings();
+            ClearCurrentScene();
+            Lightmapping.Bake();
+        }
+
+        [MenuItem("Dungeon Architect/Internal Dev Tools/Lightmap/Clear All", priority = 2021)]
+        public static void ClearAllScenes()
+        {
+            OpenAllScenes(() => ClearCurrentScene(), false);
+        }
+
+        static void OpenAllScenes(System.Action action, bool saveAfterAction)
+        {
+            if (action == null)
+            {
+                return;
+            }
+
+            var assetPaths = AssetDatabase.GetAllAssetPaths();
+            var scenePaths = new List<string>();
+            foreach (var assetPath in assetPaths)
+            {
+                if (assetPath.EndsWith(".unity") && assetPath.StartsWith("Assets/DungeonArchitect"))
+                {
+                    scenePaths.Add(assetPath);
+                }
+            }
+            foreach (var scenePath in scenePaths)
+            {
+                EditorSceneManager.OpenScene(scenePath);
+                action.Invoke();
+
+                if (saveAfterAction)
+                {
+                    EditorSceneManager.MarkAllScenesDirty();
+                    EditorSceneManager.SaveOpenScenes();
+                }
+            }
+        }
+    }
+}
