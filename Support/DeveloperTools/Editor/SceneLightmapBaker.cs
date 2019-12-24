2386eed1f07e83634e2817b6ca9613466291f953
Ali Akbar <ali.akbar@coderespawn.com>
2019-12-24 08:28:07 +0530

    Updated the lightmap baker

--- Support/DeveloperTools/Editor/SceneLightmapBaker.cs
+++ Support/DeveloperTools/Editor/SceneLightmapBaker.cs
@@ -17,14 +17,14 @@ namespace DungeonArchitect.Editors.DevTools
         }
 
 
-        [MenuItem("Dungeon Architect/Internal Dev Tools/Lightmap/Clear Current", priority = 2001)]
+        //[MenuItem("Dungeon Architect/Internal Dev Tools/Lightmap/Clear Current", priority = 2001)]
         public static void ClearCurrentScene()
         {
             Lightmapping.Clear();
             Lightmapping.ClearLightingDataAsset();
         }
 
-        [MenuItem("Dungeon Architect/Internal Dev Tools/Lightmap/Bake Current", priority = 2002)]
+        //[MenuItem("Dungeon Architect/Internal Dev Tools/Lightmap/Bake Current", priority = 2002)]
         public static void BakeCurrentScene()
         {
             SetupLightSettings();
@@ -32,10 +32,25 @@ namespace DungeonArchitect.Editors.DevTools
             Lightmapping.Bake();
         }
 
-        [MenuItem("Dungeon Architect/Internal Dev Tools/Lightmap/Clear All", priority = 2021)]
+        [MenuItem("Window/Rendering/Dungeon Architect/Internal Dev Tools/Clear Lighting on All Samples", priority = 2021)]
         public static void ClearAllScenes()
         {
-            OpenAllScenes(() => ClearCurrentScene(), false);
+            bool proceed = EditorUtility.DisplayDialog("Build Lighting", "Are you sure you want to clear lighting data on all the scenes?", "Yes", "No");
+            if (proceed)
+            {
+                OpenAllScenes(() => ClearCurrentScene(), false);
+            }
+        }
+
+
+        [MenuItem("Window/Rendering/Dungeon Architect/Internal Dev Tools/Bake Lighting on All Samples", priority = 2021)]
+        public static void BakeAllScenes()
+        {
+            bool build = EditorUtility.DisplayDialog("Build Lighting", "Are you sure you want to build lighting on all the scenes?", "Yes", "No");
+            if (build)
+            {
+                OpenAllScenes(() => BakeCurrentScene(), true);
+            }
         }
 
         static void OpenAllScenes(System.Action action, bool saveAfterAction)
