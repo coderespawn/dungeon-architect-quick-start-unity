7160a7f7f975baf9427a439701a00dcb37500efd
Ali Akbar <ali.akbar@coderespawn.com>
2019-12-26 10:36:21 +0530

    Grid flow demo working

---
+++ Support/Scripts/FPSController/DAFPSMouseLook.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DungeonArchitect.Samples.Common
+{
+    public class DAFPSMouseLook : MonoBehaviour
+    {
+        public float mouseSensitivity = 100.0f;
+        public Transform playerBody;
+
+        float angleUpDown = 0;
+
+        public void Start()
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+
+        private void Update()
+        {
+            float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+            float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+            angleUpDown -= mouseY;
+            angleUpDown = Mathf.Clamp(angleUpDown, -90, 90);
+            transform.localRotation = Quaternion.Euler(angleUpDown, 0, 0);
+
+            if (playerBody != null)
+            {
+                playerBody.Rotate(0, mouseX, 0);
+            }
+        }
+    }
+}
---
+++ Support/Scripts/FPSController/DAFPSMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DungeonArchitect.Samples.Common
+{
+    public class DAFPSMovement : MonoBehaviour
+    {
+        public float speed = 12;
+        public float gravity = -9.81f;
+        public float jumpHeight = 1;
+
+        public CharacterController controller;
+        Vector3 velocity = Vector3.zero;
+
+        void Update()
+        {
+            if (controller == null) return;
+
+            var x = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+            var z = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+
+            Vector3 move = transform.right * x + transform.forward * z;
+            controller.Move(move);
+
+            if (Input.GetButton("Jump") && controller.isGrounded)
+            {
+                //velocity.y += Mathf.Sqrt(jumpHeight * -1 * gravity);
+            }
+
+            velocity.y += gravity * Time.deltaTime;
+            var movementPerFrame = velocity * Time.deltaTime;
+            controller.Move(movementPerFrame);
+            if (controller.isGrounded)
+            {
+                velocity.y = 0;
+            }
+        }
+    }
+}
