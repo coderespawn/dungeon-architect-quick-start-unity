96486d8f077ab3ef5ab93d7115e7222279be5faa
Ali Akbar <ali.akbar@gmail.com>
2020-09-22 15:20:03 +0530

    Added snap query sample. Updated the snap 2d sample

---
+++ DemoBuilder_Snap_Query/DoorTracker.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DungeonArchitect.Builders.Snap;
+
+public class DoorTracker : MonoBehaviour
+{
+    public SnapQuery snapQuery;
+
+    void OnDrawGizmosSelected()
+    {
+        var position = transform.position;
+        SnapQueryModuleInfo moduleInfo;
+        if (snapQuery.GetModuleInfo(position, out moduleInfo))
+        {
+            var bounds = moduleInfo.instanceInfo.WorldBounds;
+            DrawGizmoBounds(bounds, Color.red);
+
+            var incomingDoors = snapQuery.GetModuleIncomingDoors(position);
+            foreach (var incomingDoor in incomingDoors)
+            {
+                var doorRenderer = incomingDoor.GetComponent<Renderer>();
+                if (doorRenderer != null)
+                {
+                    DrawGizmoBounds(doorRenderer.bounds, Color.green);
+                }
+            }
+
+            var outgoingDoors = snapQuery.GetModuleOutgoingDoors(position);
+            foreach (var outgoingDoor in outgoingDoors)
+            {
+                var doorRenderer = outgoingDoor.GetComponent<Renderer>();
+                if (doorRenderer != null)
+                {
+                    DrawGizmoBounds(doorRenderer.bounds, Color.cyan);
+                }
+            }
+
+        }
+    }
+
+    void DrawGizmoBounds(Bounds bounds, Color color)
+    {
+        Gizmos.color = color;
+        Gizmos.DrawWireCube(bounds.center, bounds.size + new Vector3(0.1f, 0.1f, 0.1f));
+    }
+}
