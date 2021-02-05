cecbd3e80e74358368ec6df37036e4693e1ffe13
Ali Akbar <ali.akbar@gmail.com>
2021-02-05 13:10:05 +0530

    Navigation sample updates at fixed intervals

--- Misc_Examples/Scripts/NavigationDemo/NavigationDemoPlayer.cs
+++ Misc_Examples/Scripts/NavigationDemo/NavigationDemoPlayer.cs
@@ -15,7 +15,7 @@ namespace DungeonArchitect.Samples.Navigation
         public float speed = 3.0F;
         public float rotateSpeed = 3.0F;
 
-        void Update()
+        void FixedUpdate()
         {
             transform.Rotate(0, Input.GetAxis("Horizontal") * rotateSpeed, 0);
             Vector3 forward = transform.TransformDirection(Vector3.forward);
