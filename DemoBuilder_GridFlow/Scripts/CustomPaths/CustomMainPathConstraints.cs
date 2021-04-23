092fa13dfac85e8bdc128638c2a5fa01e31a9971
Ali Akbar <ali.akbar@coderespawn.com>
2021-04-23 19:18:22 +0530

    New sample demoing grid flow paths created using script

---
+++ DemoBuilder_GridFlow/Scripts/CustomPaths/CustomMainPathConstraints.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DungeonArchitect;
+using UnityEngine;
+
+namespace DungeonArchitect.Samples
+{
+    public class CustomMainPathConstraints : ScriptableObject, IGridFlowLayoutNodePositionConstraint
+    {
+        public bool CanCreateNodeAt(int currentPathPosition, int totalPathLength, Vector2Int nodeCoord, Vector2Int gridSize)
+        {
+            return nodeCoord.x == 0 ||
+                   nodeCoord.y == 0 ||
+                   nodeCoord.x == gridSize.x - 1 ||
+                   nodeCoord.y == gridSize.y - 1;
+        }
+    }
+
+}
---
+++ DemoBuilder_GridFlow/Scripts/CustomPaths/CustomPathConstraintsA.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DungeonArchitect;
+using UnityEngine;
+
+namespace DungeonArchitect.Samples
+{
+    public class CustomPathConstraintsA : ScriptableObject, IGridFlowLayoutNodePositionConstraint
+    {
+        public bool CanCreateNodeAt(int currentPathPosition, int totalPathLength, Vector2Int nodeCoord, Vector2Int gridSize)
+        {
+            return nodeCoord.x == 1 ||
+                   nodeCoord.y == 1 ||
+                   nodeCoord.x == gridSize.x - 2 ||
+                   nodeCoord.y == gridSize.y - 2;
+        }
+    }
+}
