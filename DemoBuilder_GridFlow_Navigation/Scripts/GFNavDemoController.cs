dc3c90f48cbc330e35711a4cf5702b03152a85f0
Ali Akbar <ali.akbar@coderespawn.com>
2021-04-28 06:55:23 +0530

    Added grid flow navigation demo?

---
+++ DemoBuilder_GridFlow_Navigation/Scripts/GFNavDemoController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DungeonArchitect.Samples
+{
+    public class GFNavDemoController : MonoBehaviour
+    {
+        public Dungeon dungeon;
+        void Start()
+        {
+            if (dungeon != null)
+            {
+                dungeon.Build();
+            }
+        }
+    }
+}
---
+++ DemoBuilder_GridFlow_Navigation/Scripts/GFNavNPCController.cs
@@ -0,0 +1,52 @@
+﻿using DungeonArchitect.Samples.ShooterGame;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace DungeonArchitect.Samples
+{
+    public class GFNavNPCController : MonoBehaviour
+    {
+        public Transform target;
+        
+        private NavMeshAgent agent;
+        private CharacterController character;
+        void Start()
+        {
+            agent = GetComponent<NavMeshAgent>();
+            character = GetComponent<CharacterController>();
+            InvokeRepeating(nameof(SeekToTarget), 0.5f, 1.0f);
+        }
+
+        void Update()
+        {
+            if (agent != null && agent.hasPath)
+            {
+                character.SimpleMove(agent.desiredVelocity);
+            }
+        }
+        
+        void SeekToTarget()
+        {
+            if (target == null)
+            {
+                FindTarget();
+            }
+
+            if (target != null)
+            {
+                agent.SetDestination(target.position);
+            }
+        }
+
+        void FindTarget()
+        {
+            // Place your logic here.  For this sample, we search for the player in the entire map
+            
+            var player = GameObject.FindGameObjectWithTag(GameTags.Player);
+            if (player != null)
+            {
+                target = player.transform;
+            }
+        }
+    }
+}
