a34badc11ff3afc3af7bebf4e11d0c5bb0b26261
Ali Akbar <ali.akbar@coderespawn.com>
2021-06-21 09:37:22 +0530

    Added a new sample for global states

---
+++ Misc_Examples/Sample_Global_State/DungeonState.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using DungeonArchitect;
+using UnityEngine;
+
+public class DungeonState : DungeonEventListener
+{
+    public int maxAllowedItem = 2;
+
+    public int numItemsSpawned { get; set; } = 0;
+    
+    public override void OnDungeonMarkersEmitted(Dungeon dungeon, DungeonModel model, LevelMarkerList markers)
+    {
+        // Reset your state here so the next rebuild works correctly
+        numItemsSpawned = 0;
+    }
+}
---
+++ Misc_Examples/Sample_Global_State/ItemStateSelectionRule.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using DungeonArchitect;
+using UnityEngine;
+
+public class ItemStateSelectionRule : SelectorRule
+{
+    private DungeonState state;
+    
+    public override bool CanSelect(PropSocket socket, Matrix4x4 propTransform, DungeonModel model, System.Random random)
+    {
+        if (state == null)
+        {
+            state = model.gameObject.GetComponent<DungeonState>();
+        }
+        
+        if (state != null && state.numItemsSpawned < state.maxAllowedItem)
+        {
+            state.numItemsSpawned++;
+            return true;
+        }
+
+        return false;
+    }
+}
