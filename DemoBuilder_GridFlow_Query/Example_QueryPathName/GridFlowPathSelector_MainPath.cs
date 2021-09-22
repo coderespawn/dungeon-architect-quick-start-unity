3a681e727b722b65f72e232deec858e374aba3a7
Ali Akbar <ali.akbar@coderespawn.com>
2021-09-22 06:49:26 +0530

    Added grid flow query samples

---
+++ DemoBuilder_GridFlow_Query/Example_QueryPathName/GridFlowPathSelector_MainPath.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using DungeonArchitect;
+using DungeonArchitect.Builders.GridFlow;
+using DungeonArchitect.Flow.Impl.GridFlow;
+using DungeonArchitect.Utils;
+
+namespace DungeonArchitect.Samples.GridFlow
+{
+    public class GridFlowPathSelector_MainPath : SelectorRule
+    {
+        public override bool CanSelect(PropSocket socket, Matrix4x4 propTransform, DungeonModel model, System.Random random)
+        {
+            var gridFlowModel = model as GridFlowDungeonModel;
+            if (gridFlowModel == null) return false;
+
+            var query = gridFlowModel.Query;
+            if (query == null) return false;
+
+            var markerLocation = Matrix.GetTranslation(ref propTransform);
+            var pathName = query.GetPathName(markerLocation);
+
+            // Select nodes with the specified path (this was defined in the grid flow editor's `Create Path` node)
+            return pathName == "main"
+                   || pathName == "main_start"
+                   || pathName == "main";
+        }
+    }
+}
---
+++ DemoBuilder_GridFlow_Query/Example_QueryPathName/GridFlowPathSelector_TreasurePath.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using DungeonArchitect;
+using DungeonArchitect.Builders.GridFlow;
+using DungeonArchitect.Flow.Impl.GridFlow;
+using DungeonArchitect.Utils;
+
+namespace DungeonArchitect.Samples.GridFlow
+{
+    public class GridFlowPathSelector_TreasurePath : SelectorRule
+    {
+        public override bool CanSelect(PropSocket socket, Matrix4x4 propTransform, DungeonModel model, System.Random random)
+        {
+            var gridFlowModel = model as GridFlowDungeonModel;
+            if (gridFlowModel == null) return false;
+
+            var query = gridFlowModel.Query;
+            if (query == null) return false;
+
+            var markerLocation = Matrix.GetTranslation(ref propTransform);
+            var pathName = query.GetPathName(markerLocation);
+            var isRoom = query.GetRoomType(markerLocation) == GridFlowLayoutNodeRoomType.Room;
+
+            // Select nodes with the specified path (this was defined in the grid flow editor's `Create Path` node)
+            return pathName == "bonus_main" && isRoom;
+        }
+    }
+}
