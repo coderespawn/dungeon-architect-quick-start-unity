4988ac6e217f60fc5142d37bfd74682d10b2c694
Ali Akbar <ali.akbar@coderespawn.com>
2024-11-27 05:34:38 +0530

    SGF Nested city and grid samples. Added kenny art

---
+++ DemoBuilder_SimpleCity/Scripts/Rules/RandomTreeJitter.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using DungeonArchitect;
+using UnityEngine;
+using Random = System.Random;
+
+namespace DungeonArchitect.Samples.SimpleCity.Kenny
+{
+    public class RandomTreeJitter : TransformationRule
+    {
+        public override void GetTransform(PropSocket socket, DungeonModel model, Matrix4x4 propTransform, Random random,
+            out Vector3 outPosition, out Quaternion outRotation, out Vector3 outScale)
+        {
+            base.GetTransform(socket, model, propTransform, random, out outPosition, out outRotation, out outScale);
+            float jitter = 3.0f / 8.0f;
+            outPosition = new Vector3(Mathf.Cos(random.NextFloat() * Mathf.PI * 2), 0, Mathf.Sin(random.NextFloat() * Mathf.PI * 2)) * jitter;
+        }
+    }
+}
