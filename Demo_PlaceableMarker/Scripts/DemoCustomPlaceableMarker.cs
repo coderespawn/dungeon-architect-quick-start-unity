26dd76d4c7e099479444e4d736abbf3019ecd455
Ali Akbar <ali.akbar@coderespawn.com>
2024-11-27 12:57:41 +0530

    Nested sgf city and grid scene updates.  Support for placement items in sgf nested dugneosn

---
+++ Demo_PlaceableMarker/Scripts/DemoCustomPlaceableMarker.cs
@@ -0,0 +1,27 @@
+//\$ Copyright 2015-22, Code Respawn Technologies Pvt Ltd - All Rights Reserved \$//\n
+using UnityEngine;
+
+namespace DungeonArchitect.Samples
+{
+    public class DemoCustomPlaceableMarker : MonoBehaviour
+    {
+        public string markerName = "MyMarker";
+
+        void OnDrawGizmosSelected()
+        {
+            DrawGizmo(true);
+        }
+
+        void OnDrawGizmos()
+        {
+            DrawGizmo(false);
+        }
+
+        void DrawGizmo(bool selected)
+        {
+            // Draw the wireframe
+            Gizmos.color = selected ? Color.red : Color.yellow;
+            Gizmos.DrawSphere(transform.position, 0.2f);
+        }
+    }
+}
---
+++ Demo_PlaceableMarker/Scripts/DemoCustomPlaceableMarkerInjector.cs
@@ -0,0 +1,24 @@
+//\$ Copyright 2015-22, Code Respawn Technologies Pvt Ltd - All Rights Reserved \$//\n
+namespace DungeonArchitect.Samples
+{
+    public class DemoCustomPlaceableMarkerInjector : DungeonEventListener
+    {
+        public override void OnDungeonMarkersEmitted(Dungeon dungeon, DungeonModel model, LevelMarkerList markers)
+        {
+            // Grab all the placeable markers in the scene
+            var placeableMarkers = FindObjectsOfType<DemoCustomPlaceableMarker>();
+            foreach (var placeableMarker in placeableMarkers)
+            {
+                // Insert a new marker in this location
+                var marker = new PropSocket();
+                marker.Id = 0;
+                marker.SocketType = placeableMarker.markerName;
+                marker.Transform = placeableMarker.transform.localToWorldMatrix;
+                marker.gridPosition = IntVector.Zero;
+                marker.cellId = 0;
+
+                markers.Add(marker);
+            }
+        }
+    }
+}
